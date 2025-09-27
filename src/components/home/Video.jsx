7a9dcec10fd96770a3ac70018e2a021539743da3
Avirup07 <sarmisthapal29@gmail.com>
2025-09-27 18:23:57 +0530

    added / before video file path

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -4,7 +4,7 @@ const Video = () => {
   return (
     <div>
       <video className="h-screen w-screen object-cover" autoPlay loop muted playsInline>
-        <source src="videos/69496b2d.mp4" type='video/mp4' />
+        <source src="/videos/69496b2d.mp4" type='video/mp4' />
       </video>
     </div>
   );
